bfbd5907b4c46118e28e99862433f66cad79fa67
Yasmim Gerotto <ygerotto@gmail.com>
2021-12-06 15:46:57 -0300

    Rotas de entrar e cadastrar

---
+++ src/app/model/UsuarioLogin.ts
@@ -0,0 +1,13 @@
+export class UsuarioLogin{
+
+    public id: number
+    public nome: string
+    public usuario: string
+    public senha: string
+    public token: string
+    public foto: string
+    public tipo: string
+
+
+
+}
